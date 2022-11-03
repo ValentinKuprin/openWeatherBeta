5d182f2c601a3c09d7437ef73f8e5fdb0bf9bc8e
Valentin Kuprin <cricket666@yandex.ru>
2022-11-03 07:02:44 +0300

    refactored

--- src/test/java/YourNickOnGitHubTest.java
+++ src/test/java/YourNickOnGitHubTest.java
@@ -42,7 +42,6 @@ public class YourNickOnGitHubTest {
 //        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //    }
 
-
     @AfterMethod
     public void shutDown() {
         driver.quit();
